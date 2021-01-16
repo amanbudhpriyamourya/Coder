27ca87604f04a281229fbbd6fcf523c1f0e8d91d
amanbudhpriyamourya <73774081+amanbudhpriyamourya@users.noreply.github.com>
2021-01-16 12:25:35 +0530

    Add files via upload

---
+++ p3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int a,b;
+   cout<<"Enter the number a:";
+   cin>>a;
+   cout<<"Enter the number b:";
+   cin>>b;
+   cout<<"Result:"<<a+b;
+   return 0;
+}
---
+++ unary_operator_p4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter A:";
+    cin>>a;
+    cout<<"Enter B:";
+    cin>>b;
+    cout<< ++a;
+    cout<< b++;
+    cout<< b;
+
+
+
+    return 0;
+}

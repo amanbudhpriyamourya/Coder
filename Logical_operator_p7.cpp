cb5da1bc92b107b212da3557a5dbfa331a6f3cf5
amanbudhpriyamourya <73774081+amanbudhpriyamourya@users.noreply.github.com>
2021-01-16 13:21:00 +0530

    Add files via upload

---
+++ Logical_operator_p7.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+   
+    cout<<(true&&false)<<(false||true)<<!(true&&true);
+
+    return 0;
+}
---
+++ string_p9.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+   char c1[]="Great";
+   char c2[]="Learning";
+   cout<<"concatenation:"<<strcat(c1,c2)<<endl;
+   cout<<"copy:"<<strcpy(c1,c2)<<endl;
+   cout<<"lenght:"<<strlen(c1);
+
+
+
+
+    return 0;
+}
